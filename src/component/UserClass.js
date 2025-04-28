07f011b379f359f2b393cfb0e566a4a4e834805f
Samikchha-Gurung <samikchhagurung179@gmail.com>
2025-04-28 18:46:03 +0530

    updated about us page api

--- src/component/UserClass.js
+++ src/component/UserClass.js
@@ -16,7 +16,7 @@ class UserClass extends React.Component{
     async componentDidMount(){
 
         // console.log(this.props.name + "Child Component Did Mount ")
-        const data=await fetch("https://api.github.com/users/akshaymarch7")
+        const data=await fetch("https://api.github.com/users/Samikchha-gurung")
         const json= await data.json();
       
         this.setState({
